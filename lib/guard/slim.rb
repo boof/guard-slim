53ded07e8d36642d9475bad6aa91fdd4b9c6a55e
Florian Aßmann <flazy@fork.de>
2011-08-11 12:12:56 +0200

    Fixed instance variables.

--- lib/guard/slim.rb
+++ lib/guard/slim.rb
@@ -1,6 +1,7 @@
 require 'guard/guard'
-require 'guard/watchers'
+require 'guard/watcher'
 require 'slim'
+require 'fileutils'
 
 module Guard
   class Slim < Guard
@@ -8,11 +9,16 @@ module Guard
     Template = ::Slim::Template
 
     def initialize(watchers = [], options = {})
-      @root    = options.delete :root
-      @locals  = options.delete(:locals) || {}
-      @options = options.delete(:slim) || {}
+      @output_root = options.delete(:output_root) || Dir.getwd
+      @input_root  = options.delete(:input_root) || Dir.getwd
+      @context     = options.delete(:context) || Object.new
+      @slim        = options.delete(:slim) || {}
 
       super watchers, options
+    end
+
+    def start
+      UI.info 'Guard-Slim is waiting for changes...'
     end
 
     def run_all
@@ -21,26 +27,33 @@ module Guard
     def run_on_change(paths)
       paths.each do |path|
         content = render File.read(path)
-        open(build_path(path), 'w') { |file| file << content }
+        open(build_path(path), 'w') do |file|
+          @slim[:pretty] ?
+            file.puts(content) :
+            file.write(content)
+        end
+        UI.info "guard-slim: rendered #{ path } to #{ build_path path }"
       end
     end
 
     protected
 
       def build_path(path)
+        path     = File.expand_path(path).sub @input_root, @output_root
         dirname  = File.dirname path
+
+        FileUtils.mkpath dirname unless File.directory? dirname
+
         basename = File.basename path, '.slim'
         basename << '.html' if File.extname(basename).empty?
-        path     = File.join dirname, path
 
-
-        @root ? File.join(@root, dirname, basename) : File.join(dirname, basename)
+        File.join dirname, basename
       end
       def render(source)
-        Template.new( @options ) { source }.render( @locals )
+        Template.new( @slim ) { source }.render( @context )
       end
       def all_paths
-        Watchers.match_files self, Dir[ ALL ]
+        Watcher.match_files self, Dir[ ALL ]
       end
 
   end
